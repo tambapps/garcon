d31cfeed721c39f083de8528b300bdc051ae1ca0
Nelson Fonkoua <nfonkoua@weborama.com>
2022-07-25 00:33:50 +0200

    added more tests

--- src/test/groovy/com/tambapps/http/garcon/GarconTest.groovy
+++ src/test/groovy/com/tambapps/http/garcon/GarconTest.groovy
@@ -2,28 +2,61 @@ package com.tambapps.http.garcon
 
 import com.tambapps.http.hyperpoet.ErrorResponseHandlers
 import com.tambapps.http.hyperpoet.HttpPoet
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 import static org.junit.jupiter.api.Assertions.assertEquals
 
 class GarconTest {
 
+  private final HttpPoet poet = new HttpPoet(url: 'http://localhost:8081').tap {
+    errorResponseHandler = ErrorResponseHandlers.parseResponseHandler(it)
+    enableHistory(1)
+    onPreExecute = {
+      // to give time garcon to start
+      if (this.firstCall) {
+        Thread.sleep(100)
+        this.firstCall = false
+      }
+    }
+  }
+  private Garcon garcon
+  private boolean firstCall
+
+  @BeforeEach
+  void init() {
+    garcon = new Garcon()
+    firstCall = true
+  }
+
+  @AfterEach
+  void dispose() {
+    garcon.stop()
+  }
 
   @Test
   void test() {
-    Garcon garcon = new Garcon()
     garcon.serveAsync {
       get '/hello', {
         return 'Hello World'
       }
     }
-    garcon.startAsync()
-    Thread.sleep(500L)
-    def poet = new HttpPoet(url: 'http://localhost:8081/hello?p=1&a=2')
+
+    assertEquals('Hello World', poet.get('/hello'))
+    assertEquals('Hello World', poet.get('/hello?p=1&a=2'))
+  }
+  @Test
+  void testMethodNotAllowed() {
+    garcon.serveAsync {
+      get '/hello', {
+        return 'Hello World'
+      }
+    }
+
     poet.errorResponseHandler = ErrorResponseHandlers.parseResponseHandler(poet)
-    assertEquals('Hello World', (poet.get('/')))
-    assertEquals('Hello World', (poet.get('/')))
-    garcon.stop()
+    poet.delete('/hello')
+    assertEquals(405, poet.history.last().responseCode)
   }
 
 }
