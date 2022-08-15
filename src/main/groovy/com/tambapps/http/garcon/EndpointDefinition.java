ef2911e1223c75e945c1cf1e97db725774dbed8c
Nelson Fonkoua <nfonkoua@weborama.com>
2022-08-16 00:02:05 +0200

    fixed major concurrency issue

--- src/main/groovy/com/tambapps/http/garcon/EndpointDefinition.java
+++ src/main/groovy/com/tambapps/http/garcon/EndpointDefinition.java
@@ -1,38 +1,40 @@
 package com.tambapps.http.garcon;
 
-import com.tambapps.http.garcon.exception.ParsingException;
 import groovy.lang.Closure;
 import lombok.Value;
 
 @Value
 class EndpointDefinition {
 
-  Closure<?> closure;
+  // needed because we modify the delegate before calling the closure
+  ThreadLocal<Closure<?>> threadLocalClosure;
   ContentType accept;
   ContentType contentType;
 
+  public EndpointDefinition(Closure<?> closure, ContentType accept, ContentType contentType) {
+    this.threadLocalClosure = ThreadLocal.withInitial(() -> (Closure<?>) closure.clone());
+    this.accept = accept;
+    this.contentType = contentType;
+  }
+
   HttpResponse call(HttpExchangeContext context) {
     // rehydrating
+    Closure<?> closure = threadLocalClosure.get();
     closure.setDelegate(context);
     closure.setResolveStrategy(Closure.DELEGATE_FIRST);
     HttpResponse response = context.getResponse();
-    try {
-      Object returnValue = closure.call();
-      if (response.getBody() == null && returnValue != null) {
-        ContentType contentType = context.getContentType();
-        if (contentType != null) {
-          response.headers.putContentTypeHeader(contentType.getHeaderValue());
-          Closure<?> composer = context.getComposers().getAt(contentType);
-          if (composer != null) {
-            returnValue = composer.call(returnValue);
-          }
+
+    Object returnValue = closure.call();
+    if (response.getBody() == null && returnValue != null) {
+      ContentType contentType = context.getContentType();
+      if (contentType != null) {
+        response.headers.putContentTypeHeader(contentType.getHeaderValue());
+        Closure<?> composer = context.getComposers().getAt(contentType);
+        if (composer != null) {
+          returnValue = composer.call(returnValue);
         }
-        response.setBody(returnValue);
       }
-    } catch (ParsingException e) {
-      response = new HttpResponse();
-      response.setStatusCode(HttpStatus.BAD_REQUEST);
-      response.setBody(e.getMessage());
+      response.setBody(returnValue);
     }
     return response;
   }
